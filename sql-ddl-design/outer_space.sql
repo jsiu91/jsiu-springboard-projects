21eb0503bf0d46f5aff9bd158c2c9f55edf8b45c
Jonathan Siu <jsiukob@gmail.com>
2021-06-03 17:29:57 -0700

    adding sql-ddl-design exercises

---
+++ sql-ddl-design/outer_space.sql
@@ -0,0 +1,43 @@
+-- from the terminal run:
+-- psql < outer_space.sql
+
+DROP DATABASE IF EXISTS outer_space;
+
+CREATE DATABASE outer_space;
+
+\c outer_space
+
+CREATE TABLE planets
+(
+  id SERIAL PRIMARY KEY,
+  planet_name TEXT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+);
+
+CREATE TABLE orbits
+{
+  id SERIAL PRIMARY KEY,
+  galaxy_id INTEGER NOT NULL REFERENCES galaxies,
+}
+
+CREATE TABLE galaxies
+{
+  id SERIAL PRIMARY KEY,
+  moon_id INT[] REFERENCES moons,
+}
+
+CREATE TABLE moons
+{
+  id SERIAL PRIMARY KEY,
+  moon_name TEXT NOT NULL, 
+}
+
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
